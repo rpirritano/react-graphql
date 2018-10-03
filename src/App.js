1358ea0de185bcf6ea745f6aa8b6543ba35a237e
Robert Pirritano <rpirritano@gmail.com>
2018-10-03 13:49:42 -0700

    added ApolloClient to App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
   uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
 });
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app</h2>
+    </div>
+  </ApolloProvider>
+);
 
 class App extends Component {
   render() {
