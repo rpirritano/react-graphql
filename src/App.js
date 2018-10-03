8373ca9ebc850900a73cdcb1125f6e7fb3e0fba4
Robert Pirritano <rpirritano@gmail.com>
2018-10-03 13:47:20 -0700

    created GraphQL endpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://vm8mjvrnv3.lp.gql.zone/graphql"
+});
 
 class App extends Component {
   render() {
